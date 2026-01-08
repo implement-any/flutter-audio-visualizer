ad2b9623c843f698dcf97bef47908b7b8a5076b8
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2026-01-08 19:26:06 +0900

    Design(visualizer): 비쥬얼라이저 기본 화면 추가

---
+++ lib/features/visualizer/widgets/artwork_circle.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ArtworkCircle extends StatelessWidget {
+  const ArtworkCircle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Colors.black,
+    );
+  }
+}
